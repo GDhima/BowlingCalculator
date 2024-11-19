4702e313e22e7fbd0f6f1c2e8c776f7470d8875a
GDhima <gdhima26@amherst.edu>
2024-11-19 16:07:58 -0500

    Add comments to Main.java.

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+/**
+ * Main Class creates an instance of BowlingGame and starts the game.
+ */
 public class Main {
     public static void main(String[] args) {
         BowlingGame game = new BowlingGame();
